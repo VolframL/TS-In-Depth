a0ab9e1dab77b2f87497e38aa8ad1362d7e38767
Vitalii VolframBM <volfram@ukr.net>
2022-12-04 16:19:45 +0200

    home 05.02 finish

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-redeclare */
 
 showHello('greeting', 'TypeScript!');
@@ -176,6 +177,57 @@ function getProperty(book: Book, bookProperty: BookProperties ): any {
     return typeof value === 'function' ? value.name : value;
 }
 
+class ReferenceItem {
+    // title: string;
+    // year: number;
+    // constructor(newTitle: string, newYear: number) {
+    //     this.title = newTitle;
+    //     this.year = newYear;
+    //     console.log('Creating a new ReferenceItem...');
+    // }
+    #id: number;
+    private _publisher!: string;
+
+    get publisher(): string {
+        return this._publisher.toUpperCase();
+    }
+    set publisher(newPublisher: string) {
+        this._publisher = newPublisher;
+    }
+
+    static department: string = 'Research Dept.';
+
+    constructor (
+        id: number,
+        public title: string,
+        protected year: number
+    ) {
+        console.log('Creating a new ReferenceItem...');
+        this.#id = id;
+    }
+    printItem(): void {
+        // console.log(`${this.title} was published in ${this.year}`);
+        console.log(ReferenceItem.department);
+        console.log(Object.getPrototypeOf(this).constructor.department);
+    }
+
+    getID(): number {
+        return this.#id;
+    }
+}
+
+class Encyclopedia extends ReferenceItem {
+    constructor(id: number, title: string, year: number, public edition: number) {
+        super(id, title, year);
+        this.edition = edition;
+    }
+
+    override printItem(): void {
+        console.log(Object.getPrototypeOf(this).constructor.department);
+        console.log(`Edition:${this.edition} ${this.year}`);
+    }
+
+}
 // Task 02.01 Basic types
 // console.log(logFirstAvailable());
 // console.log(getBookTitlesByCategory(1));
@@ -278,55 +330,18 @@ function getProperty(book: Book, bookProperty: BookProperties ): any {
 
 
 // Task 05.01. Creating and Using Classes
-
-class ReferenceItem {
-    // title: string;
-    // year: number;
-    // constructor(newTitle: string, newYear: number) {
-    //     this.title = newTitle;
-    //     this.year = newYear;
-    //     console.log('Creating a new ReferenceItem...');
-    // }
-    private id: number;
-
-    static department: string = 'dptmt';
-
-    constructor(public title: string, protected year: number, id: number) {
-        this.title = title;
-        this.year = year;
-        this.id = id;
-    }
-    printItem(): void {
-        // console.log('title was published in year');
-        console.log(ReferenceItem.department);
-    }
-    _publisher!: string;
-    get publisher(): string {
-        return this._publisher.toUpperCase();
-    }
-    set publisher(newPublisher: string) {
-        this._publisher = newPublisher;
-    }
-    getID(): number {
-        return this.id;
-    }
-}
-
-class Encyclopedia extends ReferenceItem {
-    edition: number;
-}
-
-// const ref = new ReferenceItem('Its title', 2022, 10);
+// const ref = new ReferenceItem(1, 'Its title', 2022);
 // ref.printItem();
 // ref.publisher = 'Slava Ukraini';
 // console.log(ref.publisher);
 // console.log(ref.getID());
 // console.log(ref);
 
-const refBook = new Encyclopedia('Encyc', 2020, 3);
-refBook.printItem();
-console.log(refBook.title);
-refBook.printItem = () => {
-    console.log(ReferenceItem.department);
-    console.log(`Edition:edition ${Encyclopedia.year}`);
-};
+
+// Task 05.02. Extending Classes
+// const refBook = new Encyclopedia(1, 'Encyc', 2020, 3);
+// refBook.printItem();
+// console.log(refBook.title);
+
+
+// Task 05.03. Creating Abstract Classes
